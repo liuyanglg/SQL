fdb3908afbf52c987099f1a874751690d2151e9f
liuyanglg <liuyanglg@outlook.com>
2017-08-15 10:10:14 +0800

    1.添加定时任务

--- SQL_MySQL/从usercenter.ucenter_user_service中数据建立关系.sql
+++ SQL_MySQL/从usercenter.ucenter_user_service中数据建立关系.sql
@@ -1,33 +1,39 @@
--- 与platformkf.`portal_buyer_bind_service`的税号、服务单位ID建立关联关系
--- 创建维护表六位代码和服务单位ID关系表
-DROP TABLE
-IF EXISTS dataserver.`tb_code_serviceid`;
+#1  创建关系表tb_code_serviceid
 
+#1.1
+# 创建维护表(dataserver.`tb_cmp_card` )中六位代码(code)和(usercenter.`ucenter_user_service`)中服务单位ID(serviceid)关系表
 CREATE TABLE
 IF NOT EXISTS dataserver.`tb_code_serviceid` (
   `code`      VARCHAR(16),
   `serviceid` VARCHAR(30)
 );
 
--- 将税号转换为六位代码，然后插入到新建表
+#1.2
+# 以税号为内连接的连接条件
+# 以表usercenter.`ucenter_user_service`建立关联关系，分3种情况
 INSERT INTO dataserver.`tb_code_serviceid` (`code`, `serviceid`) (
   SELECT
     T_M.`code`,
-    T_BS.`c_serviceid`
+    T_US.`c_serviceid`
   FROM
     dataserver.`tb_cmp_card` T_M
-    JOIN platformkf.`portal_buyer_bind_service` T_BS ON T_M.`taxid` = T_BS.`c_texnum`
+    JOIN usercenter.`ucenter_user_service` T_US ON T_M.`taxid` = T_US.`c_taxnum`
   WHERE
-    T_BS.`c_serviceid` IS NOT NULL
-    AND T_BS.`c_serviceid` != ''
+    T_US.`c_serviceid` IS NOT NULL
+    AND T_US.`c_serviceid` != ''
     AND T_M.`taxid` IS NOT NULL
     AND T_M.`taxid` != ''
+    AND T_US.`dt_adddate` > (
+      SELECT MAX(T_BS.dt_adddate)
+      FROM
+        platformkf.`portal_buyer_bind_service` T_BS
+    )
 );
 
--- 创建审核表六位代码、税号和服务单位ID关系表
-DROP TABLE
-IF EXISTS dataserver.`tb_code_taxid_serviceid`;
+#2   创建关系表tb_code_taxid_serviceid
 
+#2.1
+# 创建审核表(dataserver.`tb_cmp_card_audit` )中六位代码(code)、税号(taxid)和(usercenter.`ucenter_user_service`)中服务单位ID(serviceid)关系表
 CREATE TABLE
 IF NOT EXISTS dataserver.`tb_code_taxid_serviceid` (
   `code`      VARCHAR(16),
@@ -35,36 +41,46 @@ IF NOT EXISTS dataserver.`tb_code_taxid_serviceid` (
   `serviceid` VARCHAR(30)
 );
 
--- 将查询到的数据插入到新建的表当中
--- 如果审核表code不为空,taixd为空，则根据审核表code查询维护表taxid，再根据taxid查询serviceid
+#2.2
+# 以税号为内连接的连接条件
+# 以表usercenter.`ucenter_user_service`建立关联关系，分3种情况
+
+#2.2.1
+# 审核表code不为空,taixd为空
 INSERT INTO dataserver.`tb_code_taxid_serviceid` (`code`, `taxid`, `serviceid`) (
   SELECT DISTINCT
     T_A.`code`,
     T_A.`taxid`,
-    T_BS.`c_serviceid`
+    T_US.`c_serviceid`
   FROM
     dataserver.`tb_cmp_card_audit` T_A
     JOIN dataserver.`tb_cmp_card` T_M ON T_A.`code` = T_M.`code`
-    JOIN platformkf.portal_buyer_bind_service T_BS ON T_M.`taxid` = T_BS.`c_texnum`
+    JOIN usercenter.`ucenter_user_service` T_US ON T_M.`taxid` = T_US.`c_taxnum`
   WHERE
     T_A.`code` IS NOT NULL
     AND T_A.`code` != ''
     AND T_A.`taxid` IS NULL
     AND T_M.`taxid` IS NOT NULL
-    AND T_BS.`c_serviceid` IS NOT NULL
-    AND T_BS.`c_serviceid` != ''
+    AND T_US.`c_serviceid` IS NOT NULL
+    AND T_US.`c_serviceid` != ''
+    AND T_US.`dt_adddate` > (
+      SELECT MAX(T_BS.dt_adddate)
+      FROM
+        platformkf.`portal_buyer_bind_service` T_BS
+    )
 );
 
--- 如果审核表code不为空，taxid也不为空，则根据审核表code查询维护表taxid，再根据taxid查询serviceid
+#2.2.2
+# 审核表code不为空,taixd不为空
 INSERT INTO dataserver.`tb_code_taxid_serviceid` (`code`, `taxid`, `serviceid`) (
   SELECT DISTINCT
     T_A.`code`,
     T_A.`taxid`,
-    T_BS.`c_serviceid`
+    T_US.`c_serviceid`
   FROM
     dataserver.`tb_cmp_card_audit` T_A
     JOIN dataserver.`tb_cmp_card` T_M ON T_A.`code` = T_M.`code`
-    JOIN platformkf.portal_buyer_bind_service T_BS ON T_M.`taxid` = T_BS.`c_texnum`
+    JOIN usercenter.`ucenter_user_service` T_US ON T_M.`taxid` = T_US.`c_taxnum`
   WHERE
     T_A.`code` IS NOT NULL
     AND T_A.`code` != ''
@@ -72,23 +88,34 @@ INSERT INTO dataserver.`tb_code_taxid_serviceid` (`code`, `taxid`, `serviceid`) 
     AND T_A.`taxid` != ''
     AND T_M.`taxid` IS NOT NULL
     AND T_M.`taxid` != ''
-    AND T_BS.`c_serviceid` IS NOT NULL
-    AND T_BS.`c_serviceid` != ''
+    AND T_US.`c_serviceid` IS NOT NULL
+    AND T_US.`c_serviceid` != ''
+    AND T_US.`dt_adddate` > (
+      SELECT MAX(T_BS.dt_adddate)
+      FROM
+        platformkf.`portal_buyer_bind_service` T_BS
+    )
 );
 
--- 如果审核表code为空，taxid不为空，则根据审核表taxid直接查询serviceid，以下是taxid为空的情况
+#2.2.3
+# 审核表code为空,taixd不为空
 INSERT INTO dataserver.`tb_code_taxid_serviceid` (`code`, `taxid`, `serviceid`) (
   SELECT DISTINCT
     T_A.`code`,
     T_A.`taxid`,
-    T_BS.`c_serviceid`
+    T_US.`c_serviceid`
   FROM
     dataserver.`tb_cmp_card_audit` T_A
-    JOIN platformkf.`portal_buyer_bind_service` T_BS ON T_A.`taxid` = T_BS.`c_texnum`
+    JOIN usercenter.`ucenter_user_service` T_US ON T_A.`taxid` = T_US.`c_taxnum`
   WHERE
     T_A.`code` IS NULL
     AND T_A.`taxid` IS NOT NULL
     AND T_A.`taxid` != ''
-    AND T_BS.`c_serviceid` IS NOT NULL
-    AND T_BS.`c_serviceid` != ''
+    AND T_US.`c_serviceid` IS NOT NULL
+    AND T_US.`c_serviceid` != ''
+    AND T_US.`dt_adddate` > (
+      SELECT MAX(T_BS.dt_adddate)
+      FROM
+        platformkf.`portal_buyer_bind_service` T_BS
+    )
 );
